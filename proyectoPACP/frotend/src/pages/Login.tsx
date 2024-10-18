5e4b71b111e3a7059dd9b895321df8d39bc59a05
azael <azael.carballo@gmail.com>
2024-10-18 09:31:35 +0100

    frist

---
+++ proyectoPACP/frotend/src/pages/Login.tsx
@@ -0,0 +1,24 @@
+import Typography  from "@mui/material/Typography";
+import Container  from "@mui/material/Container";
+import  Button  from "@mui/material/Button";
+function Login() {
+    return (
+      <>
+      <Container>   
+        <Typography variant="h1">Página Login Pablo Azael Carballo H1</Typography>
+        <Typography variant="h2">Página Login Pablo Azael Carballo H2</Typography>
+        <Typography variant="h3">Página Login Pablo Azael Carballo H3</Typography>
+        <Typography variant="subtitle1">Página Login Pablo Azael Carballo subtitle1</Typography>
+        <Typography variant="body1">Página Login Pablo Azael Carballo body1</Typography>
+        <Typography variant="caption">Página Login Pablo Azael Carballo caption</Typography>
+
+        <Button variant="text">text</Button>
+        <Button variant="contained">contained</Button>
+        <Button variant="outlined">outline</Button>
+     </Container>
+      </>
+    )
+  }
+  export default Login
+  
+
